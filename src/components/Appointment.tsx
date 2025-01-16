1218dcac0e199771a0ec34f77eef2dbaa35e2c97
Zorigt Gantumur <zorigt@numurcredit.mn>
2025-01-16 17:35:47 +0800

    home second

---
+++ src/components/Appointment.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Appointment: React.FC = () => {
+    return (
+        <div>
+            <h1>Appointment</h1>
+            <p>This is the Appointment component.</p>
+        </div>
+    );
+};
+
+export default Appointment;
---
+++ src/components/ProductsHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductsHome: React.FC = () => {
+    return (
+        <div>
+            <h1>Products Home</h1>
+            <p>Welcome to the Products Home page!</p>
+        </div>
+    );
+};
+
+export default ProductsHome;
