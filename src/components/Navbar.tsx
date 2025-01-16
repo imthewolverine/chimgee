08e01e488f528d49a034f08d1031c795839fbd2a
Zorigt Gantumur <zorigt@numurcredit.mn>
2025-01-16 18:06:28 +0800

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,13 @@
+'use client';
+
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <Link to="/">Logo</Link>
+        <nav className="">
+            <div className="">
+                Logo
             </div>
-            <ul className="navbar-links">
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/services">Services</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
         </nav>
     );
 };
